7d1c7e9d088f113d98efdc4166ef8622bf9ef381
Jessica Dussault <jdussault4@gmail.com>
2016-08-19 09:23:15 -0500

    changes ttl to use earlywashingtondc url

--- scripts/owl_to_ttl.rb
+++ scripts/owl_to_ttl.rb
@@ -7,8 +7,8 @@ output_file = File.join(File.dirname(__FILE__), '../rdf.oscys.objectproperties.t
 repository = RDF::Repository.load(owl_file)
 
 PREFIXES = { 
-  :osrdf => "http://cdrhsites.unl.edu/data/projects/oscys/rdf/oscys.relationships#", 
-  :oscys => "http://cdrhsites.unl.edu/data/projects/oscys/rdf/oscys.objectproperties.owl#", 
+  :osrdf => "http://earlywashingtondc.org/rdf/oscys.relationships#", 
+  :oscys => "http://earlywashingtondc.org/rdf/oscys.objectproperties.owl#", 
   :rdfs => "http://www.w3.org/2000/01/rdf-schema#",
   :owl => "http://www.w3.org/2002/07/owl#"
 }
