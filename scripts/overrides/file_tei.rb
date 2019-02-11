92711e50be5ac88f2a5eae1a96ef1826eef0cdad
Jessica Dussault <jdussault4@gmail.com>
2019-02-11 16:35:27 -0600

    imitating original personography
    small changes to text field spacing
    person with no ID is being populated with a title

---
+++ scripts/overrides/file_tei.rb
@@ -0,0 +1,29 @@
+class FileTei < FileType
+
+  # this is an override of the default XSLT behavior to transform
+  # TEI to Solr XML
+  def transform_solr
+    puts "transforming #{self.filename}"
+    req = {}
+
+    # imitating structure of tei_to_es for oscys overrides
+    # but not worrying about breaking into subdocuments for each class
+    if self.filename.include?("persons")
+      # @file_location
+      require_relative "tei_to_solr_personography.rb"
+      docs = TeiToSolrPersonography.new(@options, self.file_location, self.out_solr).xml
+    elsif self.filename.include?("caseid")
+      # caseid
+    else
+      # document
+    end
+
+
+    if @options["output"]
+      File.write(File.join(@out_solr, self.filename), docs)
+    end
+    # TODO send back any problems with an error key
+    { "doc" => docs }
+  end
+
+end
